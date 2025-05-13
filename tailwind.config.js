358457366d8458e446afc8ce170ca8385dfbf958
Ahmed Bin Yasin <ahmed@iclosed.io>
2025-05-13 12:19:47 +0500

    Ay-commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
